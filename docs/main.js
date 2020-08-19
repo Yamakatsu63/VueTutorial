3c7e5574319067b18d8203f06845611b14af75a6
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-08-19 09:04:22 +0900

    コメントをリストに追加

--- docs/main.js
+++ docs/main.js
@@ -1,24 +1,56 @@
+
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello!'
+    message: '',
+    datas: []
+  },
+  created() {
+    // インスタンス作成時に自動的に fetch() する
+    this.datas = todoStorage.fetch()
+  },
+  watch: {
+    datas: {
+      handler: function(datas){
+        todoStorage.save(datas)
+      },
+      deep: true
+    }
+  },
+  methods:{
+    add:function(event, value){
+      var comment = this.$refs.comment
+      //入力されたデータをdatasへpush
+      this.datas.push({
+        id: todoStorage.uid++,
+        data: comment.value
+      })
+      //フォーム要素を空にする
+      comment.value = ""
+    }
   }
 })
 
 Vue.component("test",{
   props: ["data"],
   template: "<li> {{ data.text }}</li>"
-})
-
-var app2 = new Vue({
-  el: '#app-2',
-  data:{
-    datas: [
-      { id: 1, text: "data1"},
-      { id: 2, text: "data2"},
-      { id: 3, text: "data3"}
-    ]
-  }
 })
 
 var app3 = new Vue({
